f74a6b69e15de4ca8c330508f7825ced2b56a580
rh516 <rrhuang99@gmail.com>
2021-04-15 19:47:45 -0400

    set up starter pack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("starting up");
+// change require to es6 import style
+import $ from 'jquery';
+import './styles.scss';
+
+const main = $('#main');
+let numSeconds = 0;
+
+function increment() {
+  numSeconds += 1;
+  main.text(`You've been on this page for ${numSeconds} seconds.`);
+}
+
+setInterval(increment, 1000);
